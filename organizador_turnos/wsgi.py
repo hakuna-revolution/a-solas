0a2f8545f9bbe198fef97a3b157f0931b4212ca6
root <root@hakuna.server>
2025-01-03 11:19:28 +0000

    Ajustes Víctor en servidor

--- organizador_turnos/wsgi.py
+++ organizador_turnos/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
+
+project_folder = os.path.expanduser('/var/www/asolas')
+load_dotenv(os.path.join(project_folder, '.env'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'organizador_turnos.settings')
-
 application = get_wsgi_application()
